2261813bfb4f1ee1e8a168225b1e23f336041824
Raguzero <14874194+Raguzero@users.noreply.github.com>
2024-11-29 18:29:31 +0100

    Change catch rate (facilitate capture Amaura and Poipole)

--- data/baseStats/amaura.asm
+++ data/baseStats/amaura.asm
@@ -6,7 +6,7 @@ db 46 ; base speed
 db 67 ; base special
 db ROCK ; species type 1
 db ICE ; species type 2
-db 45 ; catch rate
+db 145 ; catch rate
 db 72 ; base exp yield
 INCBIN "pic/bmon/amaura.pic",0,1 ; 66, sprite dimensions
 dw AmauraPicFront
--- data/baseStats/poipole.asm
+++ data/baseStats/poipole.asm
@@ -6,7 +6,7 @@ db 73 ; base speed
 db 73 ; base special
 db POISON ; species type 1
 db POISON ; species type 2
-db 45 ; catch rate
+db 145 ; catch rate
 db 189 ; base exp yield
 INCBIN "pic/bmon/poipole.pic",0,1 ; 55, sprite dimensions
 dw PoipolePicFront
